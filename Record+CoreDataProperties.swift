20a5fdb7721291d339dc44318ed5c1a7cd658034
Christian Acuña <cacuna0828@gmail.com>
2016-04-21 10:29:40 +0800

    create core data Record entity and added NSManagedObject subclass

---
+++ Record+CoreDataProperties.swift
@@ -0,0 +1,34 @@
+//
+//  Record+CoreDataProperties.swift
+//  old-China-photos
+//
+//  Created by Christian on 4/21/16.
+//  Copyright © 2016 Crossroads. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Record {
+
+    @NSManaged var latitude: Double
+    @NSManaged var longitude: Double
+    @NSManaged var primaryTitle: String?
+    @NSManaged var makerName: String?
+    @NSManaged var type: String?
+    @NSManaged var medium: String?
+    @NSManaged var place: String?
+    @NSManaged var date: String?
+    @NSManaged var source: String?
+    @NSManaged var creditLine: String?
+    @NSManaged var objectNumber: String?
+    @NSManaged var department: String?
+    @NSManaged var dimensions: String?
+    @NSManaged var culture: String?
+    @NSManaged var imageThumbURI: String?
+    @NSManaged var recordLink: String?
+
+}
---
+++ Record.swift
@@ -0,0 +1,17 @@
+//
+//  Record.swift
+//  old-China-photos
+//
+//  Created by Christian on 4/21/16.
+//  Copyright © 2016 Crossroads. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Record: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}

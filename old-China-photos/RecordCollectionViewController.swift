fb540dc4b08204e599fb0f3c8df9e4a667ee0e4b
Christian Acuña <cacuna0828@gmail.com>
2016-04-23 10:34:06 +0800

    create DetailRecordViewController and pass record to DetailRecordViewController via segue

--- old-China-photos/RecordCollectionViewController.swift
+++ old-China-photos/RecordCollectionViewController.swift
@@ -73,36 +73,16 @@ class RecordCollectionViewController: UICollectionViewController {
     
         return cell
     }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
-        return false
-    }
-
-    override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
     
+    override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        let record = records[indexPath.item]
+            performSegueWithIdentifier("MasterToDetail", sender: record)
     }
-    */
 
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "MasterToDetail" {
+            let detailRecordViewController = segue.destinationViewController as! DetailRecordViewController
+            detailRecordViewController.record = sender as? Record
+        }
+    }
 }

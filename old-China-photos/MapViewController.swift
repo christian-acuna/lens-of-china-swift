b2d0b7f23e54a258dc82a861d2e1ae2cbcc530de
Christian Acuña <cacuna0828@gmail.com>
2016-04-26 16:31:41 +0800

    center map on locations and add custom annotationView for pins that animate as they are dropped

--- old-China-photos/MapViewController.swift
+++ old-China-photos/MapViewController.swift
@@ -27,9 +27,46 @@ class MapViewController: UIViewController {
         
         cities = try! managedObjectContext.executeFetchRequest(fetchRequest) as! [City]
         mapView.addAnnotations(cities)
+        showLocations()
     }
     
-    @IBAction func showLocations(sender: AnyObject) {
+    @IBAction func showLocations() {
+        let region = regionForAnnotations(cities)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    
+    func regionForAnnotations(annotations: [MKAnnotation])
+        -> MKCoordinateRegion {
+            var region: MKCoordinateRegion
+            switch annotations.count { case 0:
+                region = MKCoordinateRegionMakeWithDistance( mapView.userLocation.coordinate, 1000, 1000)
+            case 1:
+                let annotation = annotations[annotations.count - 1]
+                region = MKCoordinateRegionMakeWithDistance(annotation.coordinate, 1000, 1000)
+            default:
+                var topLeftCoord = CLLocationCoordinate2D(latitude: -90,
+                                                          longitude: 180)
+                var bottomRightCoord = CLLocationCoordinate2D(latitude: 90,
+                                                                                                                        longitude: -180)
+                for annotation in annotations { topLeftCoord.latitude = max(topLeftCoord.latitude,
+                                                                            annotation.coordinate.latitude)
+                    topLeftCoord.longitude = min(topLeftCoord.longitude, annotation.coordinate.longitude)
+                    bottomRightCoord.latitude = min(bottomRightCoord.latitude, annotation.coordinate.latitude)
+                    bottomRightCoord.longitude = max(bottomRightCoord.longitude,annotation.coordinate.longitude)
+                }
+                
+                let center = CLLocationCoordinate2D( latitude: topLeftCoord.latitude - (topLeftCoord.latitude - bottomRightCoord.latitude) / 2, longitude: topLeftCoord.longitude - (topLeftCoord.longitude - bottomRightCoord.longitude) / 2)
+                let extraSpace = 1.3
+                let span = MKCoordinateSpan(
+                    latitudeDelta: abs(topLeftCoord.latitude - bottomRightCoord.latitude) * extraSpace,
+                    longitudeDelta: abs(topLeftCoord.longitude - bottomRightCoord.longitude) * extraSpace)
+                region = MKCoordinateRegion(center: center, span: span) }
+            return mapView.regionThatFits(region)
+    }
+    
+    func showCityDetails(sender: UIButton) {
+        
     }
     
     
@@ -39,5 +76,43 @@ class MapViewController: UIViewController {
 
 extension MapViewController: MKMapViewDelegate {
     
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        guard annotation is City else { return nil }
+        
+        let identifier = "City"
+        var annotationView = mapView.dequeueReusableAnnotationViewWithIdentifier(identifier) as! MKPinAnnotationView!
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            
+            annotationView.enabled = true
+            annotationView.canShowCallout = true
+            annotationView.animatesDrop = true
+//            annotationView.pinColor = UIColor
+            let rightButton = UIButton(type: .DetailDisclosure)
+            rightButton.addTarget(self, action: #selector(MapViewController.showCityDetails(_:)), forControlEvents: .TouchUpInside)
+            annotationView.rightCalloutAccessoryView = rightButton
+        } else {
+            annotationView.annotation = annotation
+        }
+        
+        let button = annotationView.rightCalloutAccessoryView as! UIButton
+        if let index = cities.indexOf(annotation as! City) {
+            button.tag = index
+        }
+        
+        return annotationView
+    }
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
